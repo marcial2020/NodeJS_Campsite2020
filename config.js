91d3134957908e05185f9d7c18b98c5b1c99b3b0
marcial melong <s1270701@student.mcckc.edu>
2020-08-29 22:19:00 -0500

    add files

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '795721154565516',
+        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+    }
+}
